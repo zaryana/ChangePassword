06d8c754fa06f13b9489ae24da5f4568de05f044
Max Shaposhnik <mshaposhnik@exoplatform.com>
2012-03-23 14:59:53 +0200

    CLDINT-399; removed validation queue;

--- web/admin/src/main/java/com/exoplatform/cloudworkspaces/CloudIntranetUtils.java
+++ web/admin/src/main/java/com/exoplatform/cloudworkspaces/CloudIntranetUtils.java
@@ -36,14 +36,11 @@ import org.slf4j.LoggerFactory;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.text.SimpleDateFormat;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
@@ -75,8 +72,6 @@ public class CloudIntranetUtils {
   private String                                 blackListConfigurationFolder;
 
   private String                                 maxUsersConfigurationFile;
-
-  private String                                 organizationServicePath     = "cloud-agent/rest/cloudworkspaces/organization";
 
   private static final Logger                    LOG                         = LoggerFactory.getLogger(CloudIntranetUtils.class);
 
