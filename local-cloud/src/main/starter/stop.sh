e884ed8458ee4fdcbd13352a861604d23fe40016
Max Shaposhnik <mshaposhnik@exoplatform.com>
2012-06-07 16:21:43 +0300

    CLDINT-540; stop script added for whole cloud;

--- local-cloud/src/main/starter/stop.sh
+++ local-cloud/src/main/starter/stop.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 
-#DATA = `curl -u cloudadmin:cloudadmin http://makizz.exoplatform.com.ua/rest/private/cloud-admin/info-service/server-states`
-#echo $DATA
-
-curl -s -u cloudadmin:cloudadmin 'http://makizz.exoplatform.com.ua/rest/private/cloud-admin/info-service/server-states' | sed -e 's/[{}]/''/g' | awk '{n=split($0,a,","); for (i=1; i<=n; i++) {f=split(a[i],b, ":");  print b[1]; system("./local-cloud/stop-instance.sh " b[1])  } }'
+curl -s -u cloudadmin:cloudadmin 'http://localhost:8080/rest/private/cloud-admin/info-service/server-states' | sed -e 's/[{}]/''/g' | awk '{n=split($0,a,","); for (i=1; i<=n; i++) {f=split(a[i],b, ":");  print b[1]; system("./local-cloud/stop-instance.sh " b[1])  } }'
 
 cd admin-tomcat/bin
 ./catalina.sh stop -force
