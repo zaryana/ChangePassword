c2dd51643f052aa0b1af1be3039af12363d60f0d
Max Shaposhnik <mshaposhnik@exoplatform.com>
2012-06-08 18:12:53 +0300

    CLDINT-540; starting script improvements;

--- local-cloud/src/main/starter/start.sh
+++ local-cloud/src/main/starter/start.sh
@@ -1,8 +1,55 @@
 #!/bin/bash
 
+# Unzipping platform to create template
+AS_ZIP="cloud-workspaces-platform-bundle-tomcat.zip"
+unzip ./local-cloud/$AS_ZIP -d ./local-cloud/
+
+# Starting PLF
+cd ./local-cloud/app-server-tomcat
+./start_eXo.sh
+
+# Waiting for full start
+sleep 60s
+curl --connect-timeout 900  http://localhost:8080/portal/intranet/home
+
+# Asking to create template
+ID="`curl -X POST -u cloudadmin:cloudadmin http://localhost:8080/cloud-agent/rest/cloud-agent/template-service/create-template`"
+
+# Check template OK (by length)
+LEN=$(echo ${#ID})
+if [$LEN -ne 32 ]; then
+  echo "Invalid backup ID received, exiting"
+  exit 1
+fi
+
+# Ok, now we can stop it
+./stop_eXo.sh -force
+
+# Zip a template
+zip -r -8   ../backup.zip  ./gatein/backup
+
+# Back to home
+cd ../..
+
+
+# Delete temporary bundle
+rm -rf ./local-cloud/app-server-tomcat
+
+
+# Set backup ID in admin conf
+cd admin-tomcat/exo-admin-conf
+sed -i s/cloud.admin.tenant.backup.id=NO_ID/cloud.admin.tenant.backup.id=$ID/ admin.properties
+
+# Starting admin
+cd ../..
 cd admin-tomcat/bin
 ./catalina.sh start
 
 sleep 10s
 
+# Starting first AS
 curl -X POST -u cloudadmin:cloudadmin http://localhost:8080/rest/private/cloud-admin/application-service/start-server?type=local-cloud-agent
+
+
+#That's it!
+exit 0
