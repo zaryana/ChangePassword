b5f338d2866f16483f8434d9619afab63936bd19
Max Shaposhnik <mshaposhnik@exoplatform.com>
2012-06-14 16:29:08 +0300

    CLDINT-540; rest changes in scripts;

--- local-cloud/src/main/starter/start.sh
+++ local-cloud/src/main/starter/start.sh
@@ -7,7 +7,7 @@ unzip -q ./local-cloud/$AS_ZIP -d ./local-cloud/
 rm ./local-cloud/$AS_ZIP
 
 # create default database - need host name
-mysql -hlocalhost -u$EXO_DB_USER -p$EXO_DB_PASSWORD -e "create database \`repository\`"
+mysql -hlocalhost -u$EXO_DB_USER -p$EXO_DB_PASSWORD -e "drop database if exists repository; create database repository default charset latin1 collate latin1_general_cs;"
 
 # Starting PLF
 cd ./local-cloud/app-server-tomcat
@@ -17,9 +17,24 @@ cd ./local-cloud/app-server-tomcat
 sleep 60s
 curl --connect-timeout 1800 -s  http://localhost:8080/portal/intranet/home
 
+
+# cURL helpers, first parameter it's URL of REST service
+function rest() {
+  local lpath=`pwd`
+  local curl_res="$lpath/curl.res"
+  local status=`curl -s -S -X $1 --output $curl_res --write-out %{http_code} -u $CLOUD_AGENT_USERNAME:$CLOUD_AGENT_PASSWORD $2`
+  local res=`cat $curl_res`
+ if [ $status='200'  ] ; then
+   echo $res
+ else
+   echo "ERROR: service $1 $2 returns status $status: $res"
+  exit 1
+ fi
+}
+
 # Asking to create template
 echo "Creating Tenant Template (JCR backup)"
-ID=`curl --connect-timeout 900 -s -S -X POST -u $CLOUD_AGENT_USERNAME:$CLOUD_AGENT_PASSWORD http://localhost:8080/cloud-agent/rest/cloud-agent/template-service/template`
+ID=$(rest 'POST' 'http://localhost:8080/cloud-agent/rest/cloud-agent/template-service/template')
 echo "Issued Tenant Template: $ID"
 sleep 15s
 
@@ -35,7 +50,7 @@ i=0
 timeout=240
 # wait no more 20min for a backup
 while [ -z $hasID ] && [ $i -lt $timeout ] ; do
-  IDS=`curl --connect-timeout 900 -s -S -X GET -u $CLOUD_AGENT_USERNAME:$CLOUD_AGENT_PASSWORD http://localhost:8080/cloud-agent/rest/cloud-agent/template-service/template`
+  IDS=$(rest 'GET' 'http://localhost:8080/cloud-agent/rest/cloud-agent/template-service/template')
   hasID=`expr match "$IDS" ".*\"\($ID\)\".*"`
   i=$((i + 1))
 sleep 5s
