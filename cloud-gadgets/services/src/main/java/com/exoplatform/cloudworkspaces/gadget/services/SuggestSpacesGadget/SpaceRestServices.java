62bafa0ceb1db91fd819d79a6e4b0a26b129cf1c
Clement Balay <cbalay@exoplatform.com>
2012-06-21 11:40:03 +0100

    CLDINT-617 - Implement second step of First Login Wizard (add Spaces) - New WS Rest to get all public spaces which user can join

--- cloud-gadgets/services/src/main/java/com/exoplatform/cloudworkspaces/gadget/services/SuggestSpacesGadget/SpaceRestServices.java
+++ cloud-gadgets/services/src/main/java/com/exoplatform/cloudworkspaces/gadget/services/SuggestSpacesGadget/SpaceRestServices.java
@@ -26,6 +26,7 @@ import org.exoplatform.commons.utils.ListAccess;
 import org.exoplatform.container.ExoContainerContext;
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
+import org.exoplatform.services.organization.User;
 import org.exoplatform.services.rest.impl.RuntimeDelegateImpl;
 import org.exoplatform.services.rest.resource.ResourceContainer;
 import org.exoplatform.social.core.identity.model.Identity;
@@ -270,7 +271,47 @@ public class SpaceRestServices implements ResourceContainer {
         return Response.ok("error").cacheControl(cacheControl).build();
       } 
         
+    }
+      
+    @GET
+    @Path("public")
+    public Response getPublicSpaces(@Context SecurityContext sc, @Context UriInfo uriInfo) {
+      
+      try {
+        String userId = getUserId(sc, uriInfo);
+        if(userId == null) {
+            return Response.status(HTTPStatus.INTERNAL_ERROR).cacheControl(cacheControl).build();
+        }
+        
+        SpaceService spaceService = (SpaceService) ExoContainerContext.getCurrentContainer().getComponentInstanceOfType(SpaceService.class);    
+        ListAccess<Space> publicSpaces = spaceService.getPublicSpacesWithListAccess(userId);
+       
+        JSONArray jsonArray = new JSONArray();
+
+        Space[] spaces = publicSpaces.load(0, publicSpaces.getSize());
+        if(spaces != null && spaces.length > 0) {
+          for (Space space : spaces) {
+            
+            if(space.getVisibility().equals("hidden"))
+             continue;
+            if(space.getRegistration().equals("close"))
+             continue;
+            
+            JSONObject json = new JSONObject();                    
+            json.put("name", space.getName());
+            json.put("displayName", space.getDisplayName());
+            json.put("spaceId", space.getId());     
+            jsonArray.put(json);    
+          }
+        }
+  
+        return Response.ok(jsonArray.toString(), MediaType.APPLICATION_JSON).cacheControl(cacheControl).build();
       }
+      catch (Exception e) {
+        log.error("Error in space invitation rest service: " + e.getMessage(), e);
+        return Response.ok("error").cacheControl(cacheControl).build();
+      }
+    }
 
   
   private String getUserId(SecurityContext sc, UriInfo uriInfo) {
