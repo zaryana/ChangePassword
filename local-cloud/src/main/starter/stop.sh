d9189c93714fce6393da65f8f34205c0d20442ac
Max Shaposhnik <mshaposhnik@exoplatform.com>
2012-07-20 17:25:08 +0300

    CLDINT-672; updated local-cloud stop script;

--- local-cloud/src/main/starter/stop.sh
+++ local-cloud/src/main/starter/stop.sh
@@ -2,12 +2,12 @@
 
 cd ./local-cloud
 
-curl -s -u cloudadmin:cloudadmin 'http://localhost:8080/rest/private/cloud-admin/info-service/server-states' | \
+curl -s -u cloudadmin:cloudadmin 'http://localhost:8080/rest/private/cloud-admin/instance-service/server-states' | \
 sed -e 's/[{}]/''/g' | awk '{n=split($0,a,","); for (i=1; i<=n; i++) {f=split(a[i],b, ":");  print b[1]; system("./stop-instance.sh " b[1])  } }'
 
 cd ..
 
-curl -s -u cloudadmin:cloudadmin 'http://localhost:8080/rest/private/cloud-admin/info-service/tenant-list-all' | \
+curl -s -u cloudadmin:cloudadmin 'http://localhost:8080/rest/private/cloud-admin/tenant-service/tenant-list-all' | \
 sed -e 's/["\[]/''/g' |sed -e 's/[]]/''/g' | awk '{n=split($0,a,","); for (i=1; i<=n; i++) { print a[i];  system("mysql -hlocalhost -u$EXO_DB_USER -p$EXO_DB_PASSWORD -e \"drop database " a[i] "\"") } }'
 
 cd admin-tomcat/bin
